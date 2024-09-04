d0ddeb0d311dc76792caeadb6409b58d2d963f5c
HUBERTBRICAU <73237744+HUBERTBRICAU@users.noreply.github.com>
2024-09-04 18:31:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// function langue() {
+  // document.getElementById("monimage").src = "images/drapeaux/netherlands.png";
+  // document.getElementById("monimage").style.visibility="visible" ;
+  // document.getElementById("bonjour").style.visibility="visible" ;
+  // document.getElementById("titre").innerHTML="netherlands" ;
+
+// document.getElementById("image").innerHTML="toto";
+
+// }
+
+
+
+
+function langue() {
+  document.getElementById("image").innerHTML = "Hello World";
+}
