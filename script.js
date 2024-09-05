eb6eb8085b62869c025467f8214f90d771dba89f
hubertbricau@gmail.com <hubertbricau@gmail.com>
2024-09-05 23:53:07 +0200

    0609

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
-// function langue() {
-  // document.getElementById("monimage").src = "images/drapeaux/netherlands.png";
-  // document.getElementById("monimage").style.visibility="visible" ;
-  // document.getElementById("bonjour").style.visibility="visible" ;
-  // document.getElementById("titre").innerHTML="netherlands" ;
-
-// document.getElementById("image").innerHTML="toto";
-
-// }
-
-
-
 
 function langue() {
-  // document.getElementById("image").innerHTML = "Hello World";
    document.getElementById("choix").style.visibility="visible" ;
 }
+function netherlands(){
+  document.getElementById("le_chalet").style.visibility="visible" ;
+  document.getElementById("le_chalet").innerHTML="Het chalet" ;
+  document.getElementById("titre").innerHTML="Het courtys-chalet" ;
+  document.getElementById("choix").style.visibility="hidden" ;
+  document.getElementById("swiper-wrapperhaut1").style.visibility="visible" ;
+}
+function france(){
+  document.getElementById("le_chalet").style.visibility="visible" ;
+  document.getElementById("le_chalet").innerHTML="Le chalet" ;
+  document.getElementById("titre").innerHTML="Le chalet des courtys" ;
+  document.getElementById("choix").style.visibility="hidden" ;
+  document.getElementById("swiper-wrapperhaut1").style.visibility="visible" ;
+}
+
+
+const hautSwiper = new Swiper('.haut1', {
+    // Optional parameters
+    
+    // loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    thumbs :{
+      swiper: basSwiper
+    }
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
+
+  // =================================================================
+
+
+
+  
