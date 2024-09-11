ce491a95de3d6d1e957c59fed85d82a8a3e706da
hubertbricau@gmail.com <hubertbricau@gmail.com>
2024-09-11 16:03:29 +0200

    1109

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 let langueEnCours = "français";
 function langue() {
-  // document.getElementById("choix").style.visibility = "visible";
+   document.getElementById("lesrecommendations").style.visibility = "visible";
 }
 function france() {
   langueEnCours = "français";
   console.log("langue = ", langueEnCours);
+  document.getElementById("titre").innerHTML = "Le chalet des Courtys";
   document.getElementById("recommendations").style.visibility = "hidden";
   document.getElementById("lesrecommendations").style.visibility = "visible";
   document.getElementById("lechalet").style.visibility = "visible";
   document.getElementById("lechalet").innerHTML = "Le chalet";
   document.getElementById("le_chalet").innerHTML = "Le chalet";
-  document.getElementById("titre").innerHTML = "Le chalet des Courtys";
   document.getElementById("lesvisites").innerHTML = "Les visites";
+  document.getElementById("lesVisites").innerHTML = "Les sites à visiter";
   document.getElementById("lesspecialites").innerHTML = "Les spécialités";
   document.getElementById("recomme").innerHTML = "Recommendations";
   // document.getElementById("recommendations").innerHTML =
@@ -20,13 +21,14 @@ function france() {
 function netherlands() {
   langueEnCours = "hollandais";
   console.log("langue = ", langueEnCours);
-  document.getElementById("recommendations").style.visibility = "hidden";
-  document.getElementById("lesrecommendations").style.visibility = "visible";
+  document.getElementById("titre").innerHTML = "Het Courtys-chalet";
+  // document.getElementById("recommendations").style.visibility = "hidden";
+  // document.getElementById("lesrecommendations").style.visibility = "visible";
   document.getElementById("lechalet").style.visibility = "visible";
   document.getElementById("lechalet").innerHTML = "Het chalet";
   document.getElementById("le_chalet").innerHTML = "Het chalet";
-  document.getElementById("titre").innerHTML = "Het Courtys-chalet";
   document.getElementById("lesvisites").innerHTML = "Bezoeken";
+  document.getElementById("lesVisites").innerHTML = "Sites om te bezoeken";
   document.getElementById("lesspecialites").innerHTML = "De specialiteiten";
   document.getElementById("recomme").innerHTML = "Aanbevelingen";
   // document.getElementById("swiper-wrapperhaut1").style.visibility="visible" ;
@@ -34,12 +36,12 @@ function netherlands() {
 function angleterre() {
   langueEnCours = "anglais";
   console.log("langue = ", langueEnCours);
-  document.getElementById("recommendations").style.visibility = "hidden";
-  document.getElementById("lesrecommendations").style.visibility = "visible";
+  document.getElementById("titre").innerHTML = "The Courtys Chalet";
+  // document.getElementById("recommendations").style.visibility = "hidden";
+  // document.getElementById("lesrecommendations").style.visibility = "visible";
   document.getElementById("lechalet").style.visibility = "visible";
   document.getElementById("lechalet").innerHTML = "The chalet";
   document.getElementById("le_chalet").innerHTML = "The chalet";
-  document.getElementById("titre").innerHTML = "The Courtys Chalet";
   document.getElementById("lesvisites").innerHTML = "The chalet";
   document.getElementById("lesspecialites").innerHTML = "The specialties";
   document.getElementById("recomme").innerHTML = "Recommendations";
@@ -48,34 +50,51 @@ function angleterre() {
 function chalet() {
   document.getElementById("recommendations").style.visibility = "hidden";
   document.getElementById("swiperVisites").style.visibility = "hidden";
-  document.getElementById("swiperWrapperhaut1").style.visibility = "visible";
+  document.getElementById("swiperChalet").style.visibility = "visible";
   document.getElementById("le_chalet").style.visibility = "visible";
   document.getElementById("lesVisites").style.visibility = "hidden";
+  document.getElementById("trait").style.visibility = "visible";
 }
 function visites() {
   document.getElementById("le_chalet").style.visibility = "hidden";
   document.getElementById("recommendations").style.visibility = "hidden";
   document.getElementById("swiperVisites").style.visibility = "visible";
-  document.getElementById("swiperWrapperhaut1").style.visibility = "hidden";
+  document.getElementById("swiperChalet").style.visibility = "hidden";
+  document.getElementById("lesVisites").style.visibility = "visible";
+  document.getElementById("trait").style.visibility = "visible";
 }
-function recomme() {
+
+function retour(){
+  document.getElementById("swiperVisites").style.visibility = "hidden";
+  document.getElementById("swiperChalet").style.visibility = "hidden";
+  document.getElementById("le_chalet").style.visibility = "hidden";
+  document.getElementById("lesVisites").style.visibility = "hidden";
+  document.getElementById("trait").style.visibility = "hidden";
+}
+
+function Important() {
   console.log("langue = ", langueEnCours);
+  document.getElementById("trait").style.visibility = "hidden";
   switch (langueEnCours) {
     case "français":
       document.getElementById("recommendations").style.visibility = "visible";
-      document.getElementById("swiperWrapperhaut1").style.visibility = "hidden";
+      document.getElementById("swiperChalet").style.visibility = "hidden";
+      document.getElementById("le_chalet").style.visibility = "hidden";
       document.getElementById("recommendations").innerHTML =
-        "Bienvenue au chalet des Courtys. Anne-Laure et Ludovic vous acceuillent avec plaisir et vous souhaitent un agréable séjour.";
+      "Bienvenue au chalet des Courtys Anne-Laure et Ludovic vous accueillent avec plaisir et vous souhaitent un agréable séjour.";
       break;
-    case "anglais":
-      document.getElementById("recommendations").style.visibility = "visible";
-      document.getElementById("swiperWrapperhaut1").style.visibility = "hidden";
-      document.getElementById("recommendations").innerHTML =
+      case "anglais":
+        document.getElementById("recommendations").style.visibility = "visible";
+        document.getElementById("swiperChalet").style.visibility = "hidden";
+        document.getElementById("recommendations").innerHTML =
+        document.getElementById("le_chalet").style.visibility = "hidden";
+        document.getElementById("recommendations").innerHTML =
         "Welcome to the chalet des Courtys. Anne-Laure and Ludovic welcome you with pleasure and wish you a pleasant stay.";
-      break;
-    case "hollandais":
-      document.getElementById("recommendations").style.visibility = "visible";
-      document.getElementById("swiperWrapperhaut1").style.visibility = "hidden";
+        break;
+        case "hollandais":
+          document.getElementById("recommendations").style.visibility = "visible";
+          document.getElementById("swiperChalet").style.visibility = "hidden";
+          document.getElementById("le_chalet").style.visibility = "hidden";
       document.getElementById("recommendations").innerHTML =
         "Welkom in het Courtys-chalet. Anne-Laure en Ludovic heten u met plezier welkom en wensen u veel plezier een aangenaam verblijf.";
       break;
